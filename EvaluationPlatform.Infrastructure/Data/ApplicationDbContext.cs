c86e267c5534a49ab468e59ad4da730cbe4829c9
mukulsing <mukulsinghh24@gmail.com>
2026-09-04 19:13:16 +0530

    add fluent api in applicationdbcon text inside infra

--- EvaluationPlatform.Infrastructure/Data/ApplicationDbContext.cs
+++ EvaluationPlatform.Infrastructure/Data/ApplicationDbContext.cs
@@ -16,6 +16,73 @@ namespace CheckMate.Infrastructure.Data
         public DbSet<Role> Roles => Set<Role>();
 
         public DbSet<Institute> Institutes => Set<Institute>();
-    
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Institute)
+                .WithMany(i => i.Users)
+                .HasForeignKey(u => u.InstituteId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Role)
+                .WithMany(r => r.Users)
+                .HasForeignKey(u => u.RoleId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Institute>()
+    .HasIndex(i => i.Code)
+    .IsUnique();
+
+            modelBuilder.Entity<Institute>()
+    .Property(i => i.Name)
+    .HasMaxLength(200)
+    .IsRequired();
+
+            modelBuilder.Entity<Institute>()
+                .Property(i => i.Code)
+                .HasMaxLength(50)
+                .IsRequired();
+
+            modelBuilder.Entity<Institute>()
+                .Property(i => i.Email)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            modelBuilder.Entity<Institute>()
+                .Property(i => i.Mobile)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            modelBuilder.Entity<User>()
+    .Property(u => u.FullName)
+    .HasMaxLength(200)
+    .IsRequired();
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.Email)
+                .HasMaxLength(150)
+                .IsRequired();
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.Mobile)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            modelBuilder.Entity<User>()
+                .Property(u => u.PasswordHash)
+                .IsRequired();
+
+            modelBuilder.Entity<Role>()
+    .Property(r => r.Name)
+    .HasMaxLength(50)
+    .IsRequired();
+
+            modelBuilder.Entity<Role>()
+                .Property(r => r.Description)
+                .HasMaxLength(250);
+        }
+
     }
 }
