1ddee6678204d0ecbe03c03b8d862d4584874398
Suraj Verma <surajverma8431@gmail.com>
2026-09-09 16:45:13 +0530

    add interfaces inside .application and actual repo class in .infra

---
+++ EvaluationPlatform.Application/Interfaces/Repositories/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using CheckMate.Domain.Entities;
+
+namespace CheckMate.Application.Interfaces.Repositories
+{
+    public interface IUserRepository
+    {
+
+        Task<bool> SuperAdminExistsAsync();
+
+        Task<User?> GetByEmailAsync(string email);
+
+        Task<User> AddAsync(User user);
+    }
+}
---
+++ EvaluationPlatform.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,37 @@
+﻿using CheckMate.Application.Interfaces.Repositories;
+using CheckMate.Domain.Entities;
+using CheckMate.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheckMate.Infrastructure.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> SuperAdminExistsAsync()
+        {
+            return await _context.Users
+                .AnyAsync(u => u.Role.Name == "SuperAdmin" && u.IsDeleted == false);
+        }
+
+        public async Task<User?> GetByEmailAsync(string email)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == email && !u.IsDeleted);
+        }
+
+        public async Task<User> AddAsync(User user)
+        {
+            await _context.Users.AddAsync(user);
+            await _context.SaveChangesAsync();
+
+            return user;
+        }
+    }
+}
